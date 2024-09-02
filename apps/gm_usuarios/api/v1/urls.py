02405d66a89ef5cc69d1438e2235d4479668dc96
ehanino <eha.nino@gmail.com>
2024-09-02 17:46:52 -0500

    03 Set Ofi

--- apps/gm_usuarios/api/v1/urls.py
+++ apps/gm_usuarios/api/v1/urls.py
@@ -3,9 +3,9 @@ from .views import (LoginView, LogoutView, UserAPIView)
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='token_obtain_pair'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('v1/login/', LoginView.as_view(), name='token_obtain_pair'),
+    path('v1/logout/', LogoutView.as_view(), name='logout'),
     
-    path('usuarios/', UserAPIView.as_view(), name='listar-crear-usuario'),
-    path('usuarios/<uuid:id>/', UserAPIView.as_view(), name='detalle-actualizar-eliminar-usuario'),
+    path('v1/usuarios/', UserAPIView.as_view(), name='listar-crear-usuario'),
+    path('v1/usuarios/<uuid:id>/', UserAPIView.as_view(), name='detalle-actualizar-eliminar-usuario'),
 ]
